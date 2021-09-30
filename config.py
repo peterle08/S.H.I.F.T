901bba45d0e4de66c82b7155c205362d20c9caff
Hoang Le <38596968+peterle08@users.noreply.github.com>
2021-09-29 20:50:15 -0400

    connect to mysql server

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key here'
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username="",
-        password="",
-        hostname="",
-        databasename="",
+        username="iejlrzf8pfra4pxv",
+        password="fzoe3kb3efmrmvdi",
+        hostname="dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        databasename="zrca5i7g64otwrn2",
     )
     SQLALCHEMY_POOL_RECYCLE = 58 # seconds
     SQLALCHEMY_TRACK_MODIFICATIONS = False
