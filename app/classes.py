038f0be50811789dfb3f29b3eee5f31c2435e797
Hoang Le <38596968+peterle08@users.noreply.github.com>
2021-10-10 23:18:55 -0400

    update get_started

--- app/classes.py
+++ app/classes.py
@@ -7,18 +7,16 @@ from app import db
 class Function:
     def get_started():
         user = Fetch.user_by_id(1)
-        user.set_password("#BestShift2021")
-        db.session.commit()
-        # if user == None:
-        #     user = User(username="admin", status="pending", profile_id=1)
-        #     user.set_password(user.password)
-        #     db.session.add(user)
-        #     db.session.add_all([
-        #         Profile(first_name="admin", last_name="admin", phone="0123456789", email="youremail@example.com"),
-        #         Setting(user_id=1, mode="light"),
-        #         Role(user_id=1, name="admin")
-        #     ])
-        #     db.session.commit()
+        if user == None:
+            user = User(username="admin", status="pending", profile_id=1)
+            user.set_password("#BestShift2021")
+            db.session.add(user)
+            db.session.add_all([
+                Profile(first_name="admin", last_name="admin", phone="0123456789", email="youremail@example.com"),
+                Setting(user_id=1, mode="light"),
+                Role(user_id=1, name="admin")
+            ])
+            db.session.commit()
 
 class Fetch:
     def user_by_id(user_id):
